11101b74cb3948d838761db6f419eff3d9ab936a
Filip Micic <filip.micic@gamecredits.com>
2018-06-09 16:07:44 +0200

    .gitignore added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Simple Web Application',
+    version='0.0.1',
+    packages=['simple_python_application'],
+    install_requires=[
+        'flask>=0.10.1',
+    ]
+)
